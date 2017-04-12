3ce636f93c96341e883b48f5337357b464c1c552
profjoaomoreira <joao.moreira@qi.edu.br>
2017-04-12 12:26:11 -0300

    Merge origin/master implementei a idade no programa

--- Documents/NetBeansProjects/TecladoScanner/src/tecladoscanner/TecladoScanner.java
+++ Documents/NetBeansProjects/TecladoScanner/src/tecladoscanner/TecladoScanner.java
@@ -6,6 +6,7 @@ public class TecladoScanner {
 public static void main(String[] args) {
     //aqui declaramos as variaveis que serao usadas
     String nome;
+    int idade;
     //a linha abaixo cria o objeto teclado que sera 
     //utilizado pela classe Scanner
     
@@ -13,9 +14,13 @@ public static void main(String[] args) {
     System.out.println("Digite o seu nome: ");
     // a linha abaixo captura o que foi digitado e armazena
     //na variavel nome
-    
     nome = teclado.nextLine();
-    System.out.println(nome);
+    
+    System.out.println("Digite sua idade: ");
+    idade = teclado.nextInt();
+    
+    System.out.println(" O seu nome eh "+ nome + "e a sua idade eh "+idade);
+    
     
 }
 }
