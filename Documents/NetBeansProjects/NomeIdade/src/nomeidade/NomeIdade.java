7b9eb250b01f92b146181b94893e0f91c2b4eda8
profjoaomoreira <joao.moreira@qi.edu.br>
2017-04-14 21:45:52 -0300

    Enviando programa que imprime o nome e a idade

---
+++ Documents/NetBeansProjects/NomeIdade/src/nomeidade/NomeIdade.java
@@ -0,0 +1,9 @@
+package nomeidade;
+
+public class NomeIdade {
+
+    public static void main(String[] args) {
+        System.out.println("Meu nome é João Moreira");
+        System.out.println("Tenho 42 anos de idade");
+    }
+}
